10c69debbfcb54eedf8a5aef5efc7ab04bc88ef8
Pau Costa <mico@micodev.es>
2021-11-23 23:54:51 +0100

    minor refactor and cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A small script to generate a random symetrical image
 
 ## Todo:
-  - Fix output folder creation
-  - Refactor color palette, and black biasing
+  - Fix output folder creation DONE
+  - Refactor color palette, and black biasing DONE more or less
   - Proper readme
   
--- main.py
+++ main.py
@@ -1,41 +1,52 @@
+import os
 from PIL import Image, ImageDraw
 import random
 import numpy as np
 
 
 imageSize = 16
-
-
-def create_random_array(size):
-    colorArray = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"]
-    array = []
-    for i in range((2*size)):
-        color_list = []
-        for j in range(size):
-            blackRandom = random.randint(0, 9)
-            if(blackRandom > 5):
-                color_list.append(random.choice(colorArray))
-            else:
-                color_list.append("#000000")
-        array.append(color_list)
-    return np.array(array)
+palette = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"]
 
 
 def mirror_array(array):
+    # some numpy magic that mirrors an array vertically
     return np.pad(array, ((0, 0), (0, len(array[0]))), mode="symmetric")
 
 
+def create_random_array(size, palette):
+    # Let's create a random matrix of color, with half the
+    # columns we need so it can be mirrored and become symmetric
+    array = []
+    for row in range(size):
+        color_list = []
+        for column in range(int(size/2)):
+            blackBias = random.randint(0, 9)
+            # in 10% increments, how much black is there in the image
+            if(blackBias > 5):
+                color_list.append(random.choice(palette))
+            else:
+                color_list.append("#000000")
+        array.append(color_list)
+    return mirror_array(np.array(array))
+
+
+# lets do a bunch of avatars, so we can choose from the output
 for iteration in range(100):
+    # First we create a new image, and draw object, so we can draw on to it
+    image = Image.new("RGB", (imageSize, imageSize))
+    draw = ImageDraw.Draw(image)
+    # then we create the color matrix
+    colorMatrix = create_random_array(imageSize, palette)
 
-    avatar = Image.new("RGB", (imageSize, imageSize))
-    colorArray = []
-    colorArray = mirror_array(create_random_array(int(imageSize/2)))
-    draw = ImageDraw.Draw(avatar)
-
-    for y, row in enumerate(colorArray):
+    # and draw it to the image
+    for y, row in enumerate(colorMatrix):
         for x, point in enumerate(row):
             draw.point((x, y), point)
 
-    avatar = avatar.resize((1024, 1024), resample=Image.NEAREST)
-    avatar.save('avatar{}.png'.format(iteration))
-    avatar.close()
+    # finally, we resize the image so it can be more easily used, and save it
+    image = image.resize((1024, 1024), resample=Image.NEAREST)
+    # lets be tidy and put the output in a folder
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    image.save('output/image{}.png'.format(iteration))
+    image.close()
